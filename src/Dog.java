e7f9c290cea47a06756d8a1f02282fcf924c4f05
xuyifan <xyfaqq@163.com>
2020-07-11 15:46:21 +0800

    修改Dog类

--- src/Dog.java
+++ src/Dog.java
@@ -3,9 +3,7 @@ public class Dog {
     private String name;
     private int age;
 
-    public Dog(String name, int age) {
-        this.name = name;
-        this.age = age;
+    public Dog() {
     }
 
     public String getName() {
